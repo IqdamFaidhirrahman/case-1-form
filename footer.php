437572bbbe1211e9d940800dad57e94fd1b68f95
unknown <iqdamfrahman29@gmail.com>
2024-03-19 22:40:31 +0700

    initial commit

---
+++ footer.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Footer Kalkulator</title>
+</head>
+<body>
+  <div class="footer">
+    <div class="container">
+      <div class="penjelasan">
+        <div class="judul">Kalkulator GLBB</div>
+        <div class="deskripsi">
+        Kecepatan akhir didapatkan dari rumus v = u + (a x t). 
+        Rumus ini menunjukkan bahwa hasil (kecepatan akhir) bergantung pada tiga faktor utama: 
+        kecepatan awal, percepatan, dan waktu.
+        </div>
+      </div>
+      <div class="identitas">
+        <div class="judul-anggota">Anggota Kelompok</div>
+        <div class="nama-anggota">
+          <div class="anggota-1">
+            <span>
+              <span class="anggota-1-span">Noory Azyza </span>
+              <span class="anggota-1-span2">(225150401111026)</span>
+            </span>
+          </div>
+          <div class="anggota-2">
+            <span>
+              <span class="anggota-2-span">M. Iqdam Faidhirrahman </span>
+              <span class="anggota-2-span2">(225150407111038)</span>
+            </span>
+          </div>
+          <div class="anggota-3">
+            <span>
+              <span class="anggota-3-span">Larasati Maurilla S. </span>
+              <span class="anggota-3-span2">(225150401111027)</span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="line"></div>
+    <div class="copyright">©Kelompok 7 - Pemrograman Web - SI E</div>
+  </div>  
+</body>
---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Header Kalkulator GLBB</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqx61gVWgIGe33WNlDCW9V9zc9nt60H5L9WzsX+Y53mDD+Gl6wX61155M" crossorigin="anonymous">
+</head>
+<body>
+    <div class="header">
+        <div class=h1>Kalkulator GLBB</div>
+        <div class=h4>Kelompok 7 - PEMWEB SI E</div>
+    </div>                
+</body>
+</html>
